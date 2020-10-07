a42ce1471a9882b86f918ba2e055b2ef6f5bca7b
evgenij.vinogradov <evgenij.vinogradov@softswiss.com>
2020-10-07 17:19:10 +0300

    add login and sign up

--- app/controllers/base_pages_controller.rb
+++ app/controllers/base_pages_controller.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 class BasePagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def about
-  end
+  def about; end
 
-  def contacts
-  end
+  def contacts; end
 
-  def help
-  end
+  def help; end
 end
--- test/controllers/base_pages_controller_test.rb
+++ test/controllers/base_pages_controller_test.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class BasePagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get base_pages_home_url
     assert_response :success
   end
 
-  test "should get about" do
+  test 'should get about' do
     get base_pages_about_url
     assert_response :success
   end
 
-  test "should get contacts" do
+  test 'should get contacts' do
     get base_pages_contacts_url
     assert_response :success
   end
 
-  test "should get help" do
+  test 'should get help' do
     get base_pages_help_url
     assert_response :success
   end
-
 end
